067b0be45fc6317fefbfeec1e9086c5ba89809f0
Sean Wiseman <seanwiseman2012@gmail.com>
2017-12-06 14:49:24 +0000

    Uploaded to PyPi

--- setup.py
+++ setup.py
@@ -3,20 +3,18 @@ from setuptools import setup
 import proofreader
 
 setup(
-    name='proofreader-python',
+    name='proofreader',
     version=proofreader.__version__,
     description='Utility for running both Pylint & Flake8 on targets whilst providing '
                 'global, though overridable, defaults.',
     packages=['proofreader', 'proofreader.config'],
-    package_data={
-        'proofreader/config': '.*'
-    },
+    include_package_data=True,
     license='MIT',
     url='https://github.com/elifesciences/proofreader-python.git',
     maintainer='eLife Sciences Publications Ltd.',
     maintainer_email='tech-team@elifesciences.org',
     classifiers=[
-        "Development Status :: 1 - Alpha",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
